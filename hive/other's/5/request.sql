3334af9179a9635ccae0df4ab1cd0d8ebdcdb7a7
Masoudvahid <83933253+Masoudvahid@users.noreply.github.com>
2023-04-18 12:12:17 +0300

    Delete hive/other's directory

--- hive/other's/5/request.sql
+++
@@ -1,9 +0,0 @@
-ADD jar /opt/cloudera/parcels/CDH/lib/hive/lib/hive-serde.jar;
-ADD jar /opt/cloudera/parcels/CDH/lib/hive/lib/hive-contrib.jar;
-
-USE podkidysheval;
-
-SELECT TRANSFORM(ip, date_time, http_request, size, http_status, browser)
-USING "sed -r 's|.ru/|.com/|'" AS ip, date_time, http_request, size, http_status, browser
-FROM Logs
-LIMIT 10;
